1d6936cbc0743360525dcbd10df9a170ee239d23
Noah Soubliere <nsoubliere@lcs.on.ca>
2022-05-06 14:53:00 -0400

    Initial Commit

---
+++ MusicMayhem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicMayhem
+//
+//  Created by Noah Alexandre Soubliere on 2022-05-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MusicMayhem/MusicMayhemApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicMayhemApp.swift
+//  MusicMayhem
+//
+//  Created by Noah Alexandre Soubliere on 2022-05-06.
+//
+
+import SwiftUI
+
+@main
+struct MusicMayhemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
